6435fa5512620514fed73257bc45e21e8e0dd43c
ArtSun <parafo@gmail.com>
2018-05-15 18:28:22 +0300

    3

--- etc/hello.py
+++ etc/hello.py
@@ -1,2 +1,11 @@
-pythonpath = '/home/bix/web'
-bind = '0.0.0.0:8080'
+CONFIG = {
+    'mode': 'wsgi',
+    'working_dir': '/home/box/web/hello.py',
+    # 'python': '/usr/bin/python',
+    'args': (
+        '--bind=0.0.0.0:8080',
+        '--workers=16',
+        '--timeout=60',
+        'hello:app',
+    ),
+}
