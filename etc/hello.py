b28f3def2a17b49de65d4073c18156957f6af1cd
ArtSun <parafo@gmail.com>
2018-05-15 18:13:34 +0300

    2

--- etc/hello.py
+++ etc/hello.py
@@ -1,12 +1,2 @@
-CONFIG = {
-    'mode': 'wsgi',
-    'working_dir': '/home/box/web',
-    # 'python': '/usr/bin/python',
-    'args': (
-        '--bind=0.0.0.0:8080',
-        '--daemon',
-        '--workers=2',
-        '--timeout=60',
-        'hello:app',
-    ),
-}
+pythonpath = '/home/bix/web'
+bind = '0.0.0.0:8080'
--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+#gunicorn.conf
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
+sudo gunicorn -b 0.0.0.0:8080 hello:app &
 sudo /etc/init.d/gunicorn restart
